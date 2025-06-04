feb87f05726d90d95ad136b050b2a5a81e02b28b
Lorrany Estrela <lorranyestrela789@gmail.com>
2025-06-04 09:21:21 -0300

    Projeto da faculdade

---
+++ aplicacao/aplicacao/src/main/java/com/ltpo/aplicacao/repositories/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.ltpo.aplicacao.repositories;
+
+import com.ltpo.aplicacao.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByEmail(String email);
+    boolean existsByEmail(String email);
+}
---
+++ aplicacao/aplicacao/src/main/java/com/ltpo/aplicacao/services/UsuarioService.java
@@ -0,0 +1,30 @@
+package com.ltpo.aplicacao.services;
+
+import com.ltpo.aplicacao.models.Usuario;
+import com.ltpo.aplicacao.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario cadastrar(Usuario usuario) {
+        if (usuarioRepository.existsByEmail(usuario.getEmail())) {
+            throw new RuntimeException("Email já cadastrado");
+        }
+
+        // Criptografar senha em aplicação real
+        return (Usuario) usuarioRepository.save(usuario);
+    }
+
+    public List listarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    // Outros métodos como buscar, atualizar, excluir
+}
+
